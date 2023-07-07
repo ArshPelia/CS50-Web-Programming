d41d9b0b8521a7f7145d16c35d62e496177a7a84
arshjit pelia <arsh.pelia1@gmail.com>
2023-07-06 22:22:36 -0400

    push before working on links

--- P1-Wiki/encyclopedia/views.py
+++ P1-Wiki/encyclopedia/views.py
@@ -2,7 +2,12 @@ from django.shortcuts import render
 
 from . import util
 
+""" 
+we pass a third argument into the render function here, one that is known as the context. 
+    In this context, we can provide information that we would like to have available within 
+    our HTML files. This context takes the form of a Python dictionary.
 
+ """
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
