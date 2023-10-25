187636f16457f80daf4873388f2885df200aba8a
arshjit pelia <arsh.pelia1@gmail.com>
2023-10-25 19:40:04 -0400

    re-imported p4; created models

--- P4/network/admin.py
+++ P4/network/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import *
+
+# Register your models here.
+admin.site.register(User)
+admin.site.register(Post)
+admin.site.register(Like)
+admin.site.register(Comment)
--- P4/network/models.py
+++ P4/network/models.py
@@ -3,4 +3,28 @@ from django.db import models
 
 
 class User(AbstractUser):
-    pass
+    followers = models.ManyToManyField('self', symmetrical=False,
+                                       blank=True, related_name='followers')
+
+    def count_followers(self):
+        return self.followers.count()
+
+    def count_following(self):
+        return User.objects.filter(followers=self).count()
+
+
+class Post(models.Model):
+    author = models.ForeignKey("User", on_delete=models.CASCADE)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    content = models.TextField(blank=False)
+
+
+class Like(models.Model):
+    Post = models.ForeignKey("Post", on_delete=models.CASCADE)
+    author = models.ForeignKey("User", on_delete=models.CASCADE)
+
+
+class Comment(models.Model):
+    Post = models.ForeignKey("Post", on_delete=models.CASCADE)
+    author = models.ForeignKey("User", on_delete=models.CASCADE)
+    content = models.TextField(blank=False)
